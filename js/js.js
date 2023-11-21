6febd3a9520d670c16eea58276706a997cd69ae8
Luis Maraboli <l.maraboliq@gmail.com>
2023-11-21 01:07:03 -0300

    subimos primera pagina web

---
+++ js/js.js
@@ -0,0 +1,26 @@
+document.getElementById("infoBtn1").addEventListener("click", function() {
+    var infoContent1 = document.getElementById("infoContent1");
+    if (infoContent1.style.display == "none") {
+        infoContent1.style.display = "block";
+    } else {
+        infoContent1.style.display = "none";
+    }
+});
+
+document.getElementById("infoBtn2").addEventListener("click", function() {
+    var infoContent2 = document.getElementById("infoContent2");
+    if (infoContent2.style.display == "none") {
+        infoContent2.style.display = "block";
+    } else {
+        infoContent2.style.display = "none";
+    }
+});
+
+document.getElementById("infoBtn3").addEventListener("click", function() {
+    var infoContent3 = document.getElementById("infoContent3");
+    if (infoContent3.style.display == "none") {
+        infoContent3.style.display = "block";
+    } else {
+        infoContent3.style.display = "none";
+    }
+});
